35c1817496964233b2202e09edd0ed56757f4468
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2020-12-11 18:53:33 +0100

    Started Http Server

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const { Console } = require("console");
 const http = require("http")
+var url = require('url');
 
 const requestListener = function (req, res) {
-    
-    Console.log(req)
+    var data = ""
+    req.on('data', d => {
+        data = data + d
+    })
+    req.on('end', () => {
+        //console.log(req)
+        console.log("Method: " + req.method)
+        console.log("Data: " + data)
+        console.log("Path: " + url.parse(req.url).pathname)
+        if (url.parse(req.url).pathname == "/position" && req.method == "PUT") {
+            console.log("return 200")
+            res.writeHead(200)
+            res.end()
+        }
+        else {
+            res.writeHead(404)
+            res.end()
+        }
+    })
 
-    res.writeHead(200)
-    res.end()
+
 }
 
 const server = http.createServer(requestListener)
