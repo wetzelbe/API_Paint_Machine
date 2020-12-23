f93e6475ba2e6baf66ade9e86ca7567920479e98
wetzelbe <benedict.f.wetzel@gmx.de>
2020-12-23 22:09:08 +0100

    Added Serial

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const http = require("http")
+const serial = require("serialport")
 var url = require('url');
 
+/*serial.list().then(ports => {
+	ports.forEach(function(port) {
+		console.log(port.path)
+		console.log(port.pnpId)
+		console.log(port.manufacturer)
+	})
+})
+*/
+var filo = []
+var ready = false
+
+var portName = process.argv[2]
+
+console.log("Serial Port in Use is: " + portName)
+var port = new serial(portName, 9600)
+var Readline = require('@serialport/parser-readline')
+var parser = new Readline({delimiter: '\r\n'})
+var pipe = port.pipe(parser)
+port.on('open', () => {
+	port.write("INIT\n")
+	ready = true
+})
+pipe.on('data', d => {
+	console.log("[Serial] " + d)
+	if (d == "Servo up")
+	{
+		port.write("INIT\n")
+		port.drain(error => {console.log(error)})
+	}
+	if (d == "-----")
+		ready = true;
+})
+port.on('close', () => {
+	console.log("Port closed")
+	ready = false
+})
 const requestListener = function (req, res) {
     var data = ""
     req.on('data', d => {
@@ -15,6 +52,7 @@ const requestListener = function (req, res) {
             console.log("return 200")
             res.writeHead(200)
             res.end()
+	    filo.push(data)
         }
         else {
             res.writeHead(404)
@@ -27,3 +65,24 @@ const requestListener = function (req, res) {
 
 const server = http.createServer(requestListener)
 server.listen(4040)
+function senddata()
+{
+	if (ready && filo.length > 0)
+	{
+	    let input = JSON.parse(filo[0])
+	    filo.shift()
+	    ready = false
+	    if (input.Touching)
+	    {
+		port.write("DOWN\n")
+	    }
+	    else
+	    {
+		port.write("UP\n")
+	    }
+	    port.drain(error => {console.log(error)})
+	    port.write("G1 X" + Math.round(input.LastknownPosition.X) + " Y" + Math.round(input.LastknownPosition.Y) + "\n")
+	    port.drain(error => {console.log(error)})
+	}
+}
+setInterval(senddata, 500)
